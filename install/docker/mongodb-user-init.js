816f7fa484fd8138f63a7c455823d3ef75cacf89
Mustafa Korkmaz <mustafa.korkmaz@linkmobility.com>
2026-02-27 16:28:56 +0300

    use env vars

--- install/docker/mongodb-user-init.js
+++ install/docker/mongodb-user-init.js
@@ -1,1 +1,15 @@
-db.createUser( { user: 'nodebb', pwd: 'nodebb', roles: [ { role: 'readWrite', db: 'nodebb' }, { role: 'clusterMonitor', db: 'admin' } ] } )
+const env = (typeof process !== 'undefined' && process.env) ? process.env : {};
+
+const appDb = env.MONGO_INITDB_DATABASE || 'forum';
+const appUser = env.MONGO_INITDB_ROOT_USERNAME || 'forum';
+const appPassword = env.MONGO_INITDB_ROOT_PASSWORD || 'forum';
+
+db = db.getSiblingDB(appDb);
+db.createUser({
+	user: appUser,
+	pwd: appPassword,
+	roles: [
+		{ role: 'readWrite', db: appDb },
+		{ role: 'clusterMonitor', db: 'admin' },
+	],
+});
